766fe871967fd52a6626442377f72164f7cc81ef
vikramshekhawat <vikramvijander26@gmail.com>
2018-01-12 13:31:58 +0530

    Maven Commit

--- src/com/bathwater/services/StripeService.java
+++ src/com/bathwater/services/StripeService.java
@@ -36,216 +36,238 @@ import com.stripe.model.Subscription;
  */
 public class StripeService {
 
-    static {
-        Stripe.apiKey = "sk_test_Pj6izShu4KwPk1EHOXcCQSH8";//"1".equals(System.getProperty("PRODUCTION_MODE")) ? System.getProperty("STRIPE") : System.getProperty("STRIPE_TEST");
-    }
-    
-    public static String createUser(String token, String emailAddress) throws StripeException {
-        Map<String, Object> customerParams = new HashMap<>();
-        customerParams.put("description", "Customer object for " + emailAddress);
-        customerParams.put("source", token);
-        
-        Customer customer = (Customer) Customer.create(customerParams);
-        
-        return customer.getId();
-    }
+	static {
+		Stripe.apiKey = "1".equals(System.getProperty("PRODUCTION_MODE")) ? System.getProperty("STRIPE")
+				: System.getProperty("STRIPE_TEST");
+	}
 
-    public static String createUser(String userid, String email, String cardNumber, String expMonth, String expYear, String cvc, String token) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
-        Map<String, Object> customerParams = new HashMap<>();
+	public static String createUser(String token, String emailAddress) throws StripeException {
+		Map<String, Object> customerParams = new HashMap<>();
+		customerParams.put("description", "Customer object for " + emailAddress);
+		customerParams.put("source", token);
 
-        customerParams.put("description", "Customer object for " + userid);
-        customerParams.put("email", email);
+		Customer customer = (Customer) Customer.create(customerParams);
 
-        if (token != null && !token.equals("")) {
-            customerParams.put("source", token);
-        } else {
-            Map<String, String> cardDetails = new HashMap<>();
+		return customer.getId();
+	}
 
-            cardDetails.put("object", "card");
-            cardDetails.put("exp_month", expMonth);
-            cardDetails.put("exp_year", expYear);
-            cardDetails.put("number", cardNumber);
-            if (cvc != null && !cvc.equals("")) {
-                cardDetails.put("cvc", cvc);
-            }
-            customerParams.put("source", cardDetails);
-        }
+	public static String createUser(String userid, String email, String cardNumber, String expMonth, String expYear,
+			String cvc, String token) throws AuthenticationException, InvalidRequestException, APIConnectionException,
+			CardException, APIException {
+		Map<String, Object> customerParams = new HashMap<>();
 
-        Customer customer = Customer.create(customerParams);
+		customerParams.put("description", "Customer object for " + userid);
+		customerParams.put("email", email);
 
-        return customer.getId();
-    }
+		if (token != null && !token.equals("")) {
+			customerParams.put("source", token);
+		} else {
+			Map<String, String> cardDetails = new HashMap<>();
 
-    public static String getDefaultCardId(String customerId) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
-        Customer customer = Customer.retrieve(customerId);
+			cardDetails.put("object", "card");
+			cardDetails.put("exp_month", expMonth);
+			cardDetails.put("exp_year", expYear);
+			cardDetails.put("number", cardNumber);
+			if (cvc != null && !cvc.equals("")) {
+				cardDetails.put("cvc", cvc);
+			}
+			customerParams.put("source", cardDetails);
+		}
 
-        return customer.getDefaultSource();
-    }
+		Customer customer = Customer.create(customerParams);
 
-    public static String updateCustomerCreditCard(String customerId, String cardNumber, String expMonth, String expYear, String cvc) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
+		return customer.getId();
+	}
 
-        Customer customer = Customer.retrieve(customerId);
+	public static String getDefaultCardId(String customerId) throws AuthenticationException, InvalidRequestException,
+			APIConnectionException, CardException, APIException {
+		Customer customer = Customer.retrieve(customerId);
 
-        Map<String, Object> updateParams = new HashMap<>();
-        Map<String, String> cardDetails = new HashMap<>();
+		return customer.getDefaultSource();
+	}
 
-        cardDetails.put("object", "card");
-        cardDetails.put("exp_month", expMonth);
-        cardDetails.put("exp_year", expYear);
-        cardDetails.put("number", cardNumber);
-        if (cvc != null && !cvc.equals("")) {
-            cardDetails.put("cvc", cvc);
-        }
-        updateParams.put("source", cardDetails);
-        customer.update(updateParams);
+	public static String updateCustomerCreditCard(String customerId, String cardNumber, String expMonth, String expYear,
+			String cvc) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException,
+			APIException {
 
-        return customer.getId();
-    }
+		Customer customer = Customer.retrieve(customerId);
 
-    public static String addNewCard(String customerId, String cardNumber, String expMonth, String expYear, String cvc) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
-        Customer customer = Customer.retrieve(customerId);
-        Map<String, String> cardDetails = new HashMap<>();
+		Map<String, Object> updateParams = new HashMap<>();
+		Map<String, String> cardDetails = new HashMap<>();
 
-        cardDetails.put("object", "card");
-        cardDetails.put("exp_month", expMonth);
-        cardDetails.put("exp_year", expYear);
-        cardDetails.put("number", cardNumber);
-        if (!StringUtil.isBlank(cvc)) {
-            cardDetails.put("cvc", cvc);
-        }
+		cardDetails.put("object", "card");
+		cardDetails.put("exp_month", expMonth);
+		cardDetails.put("exp_year", expYear);
+		cardDetails.put("number", cardNumber);
+		if (cvc != null && !cvc.equals("")) {
+			cardDetails.put("cvc", cvc);
+		}
+		updateParams.put("source", cardDetails);
+		customer.update(updateParams);
 
-        Map<String, Object> createParams = new HashMap<>();
-        createParams.put("source", cardDetails);
-        Card card = (Card) customer.getSources().create(createParams);
+		return customer.getId();
+	}
 
-        return card.getId();
-    }
-    
-    public static String addNewCard(String customerId, String token) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
-        Customer customer = Customer.retrieve(customerId);
-        Map<String, Object> params = new HashMap<>();
-        params.put("source", token);
-        
-        Card card = (Card) customer.getSources().create(params);
-        
-        return card.getId();
-    }
-    
-    public static boolean deleteCard(String customerId, String cardId) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
-        Customer customer = Customer.retrieve(customerId);
-        Card card = (Card) customer.getSources().retrieve(cardId);
-        DeletedCard deletedCard = card.delete();
-        return deletedCard.getDeleted();
-    }
+	public static String addNewCard(String customerId, String cardNumber, String expMonth, String expYear, String cvc)
+			throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException,
+			APIException {
+		Customer customer = Customer.retrieve(customerId);
+		Map<String, String> cardDetails = new HashMap<>();
 
-    public static String createSubscription(String customerId, String planId, String couponCode) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
-        Customer customer = Customer.retrieve(customerId);
-        Map<String, Object> params = new HashMap<>();
-        params.put("plan", planId);
-        if (couponCode != null && !couponCode.equals("")) {
-            params.put("coupon", couponCode);
-        }
-        Subscription subscription = customer.createSubscription(params);
-        return subscription.getId();
-    }
+		cardDetails.put("object", "card");
+		cardDetails.put("exp_month", expMonth);
+		cardDetails.put("exp_year", expYear);
+		cardDetails.put("number", cardNumber);
+		if (!StringUtil.isBlank(cvc)) {
+			cardDetails.put("cvc", cvc);
+		}
 
-    public static boolean deleteSubscription(String customerId, String subscriptionId) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
+		Map<String, Object> createParams = new HashMap<>();
+		createParams.put("source", cardDetails);
+		Card card = (Card) customer.getSources().create(createParams);
 
-        Customer customer = Customer.retrieve(customerId);
+		return card.getId();
+	}
 
-        for (Subscription subscription : customer.getSubscriptions().getData()) {
-            if (subscription.getId().equals(subscriptionId)) {
-                subscription.cancel(null);
-                return true;
-            }
-        }
+	public static String addNewCard(String customerId, String token) throws AuthenticationException,
+			InvalidRequestException, APIConnectionException, CardException, APIException {
+		Customer customer = Customer.retrieve(customerId);
+		Map<String, Object> params = new HashMap<>();
+		params.put("source", token);
 
-        return false;
-    }
+		Card card = (Card) customer.getSources().create(params);
 
-    public static String createMembershipPlan(String planId, String planName, String amount, String interval) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException, InvalidRequestException {
+		return card.getId();
+	}
 
-        HashMap<String, Object> planParams = new HashMap<>();
+	public static boolean deleteCard(String customerId, String cardId) throws AuthenticationException,
+			InvalidRequestException, APIConnectionException, CardException, APIException {
+		Customer customer = Customer.retrieve(customerId);
+		Card card = (Card) customer.getSources().retrieve(cardId);
+		DeletedCard deletedCard = card.delete();
+		return deletedCard.getDeleted();
+	}
 
-        planParams.put("amount", amount);
-        planParams.put("interval", interval);
-        planParams.put("name", planName);
-        planParams.put("currency", "usd");
-        planParams.put("id", planId);
+	public static String createSubscription(String customerId, String planId, String couponCode)
+			throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException,
+			APIException {
+		Customer customer = Customer.retrieve(customerId);
+		Map<String, Object> params = new HashMap<>();
+		params.put("plan", planId);
+		if (couponCode != null && !couponCode.equals("")) {
+			params.put("coupon", couponCode);
+		}
+		Subscription subscription = customer.createSubscription(params);
+		return subscription.getId();
+	}
 
-        Plan createdPlan = Plan.create(planParams);
+	public static boolean deleteSubscription(String customerId, String subscriptionId) throws AuthenticationException,
+			InvalidRequestException, APIConnectionException, CardException, APIException {
 
-        return createdPlan.getId();
-    }
+		Customer customer = Customer.retrieve(customerId);
 
-    public static boolean deleteMembershipPlan(String planId) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
-        Plan plan = Plan.retrieve(planId);
+		for (Subscription subscription : customer.getSubscriptions().getData()) {
+			if (subscription.getId().equals(subscriptionId)) {
+				subscription.cancel(null);
+				return true;
+			}
+		}
 
-        DeletedPlan deletedPlan = plan.delete();
+		return false;
+	}
 
-        return deletedPlan.getDeleted();
-    }
+	public static String createMembershipPlan(String planId, String planName, String amount, String interval)
+			throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException,
+			APIException, InvalidRequestException {
 
-    public static String listAllPlans() throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
-        return Plan.list(new HashMap<String, Object>()).toString();
-    }
+		HashMap<String, Object> planParams = new HashMap<>();
 
-    public static boolean validateCoupon(String couponID) {
-        try {
-            Coupon coupon = Coupon.retrieve(couponID);
-            return coupon != null;
-        } catch (CardException | APIException | AuthenticationException | InvalidRequestException | APIConnectionException ex) {
-            return false;
-        }
-    }
-    
-    public static ReferralCodeValidStatus populateCouponData(ReferralCodeValidStatus status, String couponID) {
-        try {
-            SimpleDateFormat formatter = new SimpleDateFormat("MM.dd.yyyy kk:mm:ss");
-            Coupon coupon = Coupon.retrieve(couponID);
-            status.setAmountOff((int) (long)coupon.getAmountOff());
-            status.setPercentOff(coupon.getPercentOff());
-            status.setRedeemBy(formatter.format(new Date(coupon.getRedeemBy() * 1000)));
-        } catch (AuthenticationException | InvalidRequestException | APIConnectionException | CardException | APIException ex) {
-            Logger.getLogger(StripeService.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return status;
-    }
+		planParams.put("amount", amount);
+		planParams.put("interval", interval);
+		planParams.put("name", planName);
+		planParams.put("currency", "usd");
+		planParams.put("id", planId);
 
-    public static String charge(Integer amount, String cardNumber, String expMonth, String expYear, String customerID) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
+		Plan createdPlan = Plan.create(planParams);
 
-        Map<String, Object> params = new HashMap<>();
-        params.put("amount", amount);
+		return createdPlan.getId();
+	}
 
-        if (customerID != null) {
-            params.put("customer", customerID);
-        } else {
-            Map<String, String> card = new HashMap<>();
-            card.put("object", "card");
-            card.put("number", cardNumber);
-            card.put("exp_year", expYear);
-            card.put("exp_month", expMonth);
-            params.put("source", card);
-        }
+	public static boolean deleteMembershipPlan(String planId) throws AuthenticationException, InvalidRequestException,
+			APIConnectionException, CardException, APIException {
+		Plan plan = Plan.retrieve(planId);
 
-        params.put("currency", "usd");
+		DeletedPlan deletedPlan = plan.delete();
 
-        Charge charge = Charge.create(params);
-        return charge.getStatus();
-    }
+		return deletedPlan.getDeleted();
+	}
 
-    public static String charge(Integer amount, String customerId, String cardId) throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
-        Map<String, Object> params = new HashMap<>();
-        params.put("amount", amount);
+	public static String listAllPlans() throws AuthenticationException, InvalidRequestException, APIConnectionException,
+			CardException, APIException {
+		return Plan.list(new HashMap<String, Object>()).toString();
+	}
 
-        params.put("customer", customerId);
-        
-        params.put("card", cardId);
+	public static boolean validateCoupon(String couponID) {
+		try {
+			Coupon coupon = Coupon.retrieve(couponID);
+			return coupon != null;
+		} catch (CardException | APIException | AuthenticationException | InvalidRequestException
+				| APIConnectionException ex) {
+			return false;
+		}
+	}
 
-        params.put("currency", "usd");
+	public static ReferralCodeValidStatus populateCouponData(ReferralCodeValidStatus status, String couponID) {
+		try {
+			SimpleDateFormat formatter = new SimpleDateFormat("MM.dd.yyyy kk:mm:ss");
+			Coupon coupon = Coupon.retrieve(couponID);
+			status.setAmountOff((int) (long) coupon.getAmountOff());
+			status.setPercentOff(coupon.getPercentOff());
+			status.setRedeemBy(formatter.format(new Date(coupon.getRedeemBy() * 1000)));
+		} catch (AuthenticationException | InvalidRequestException | APIConnectionException | CardException
+				| APIException ex) {
+			Logger.getLogger(StripeService.class.getName()).log(Level.SEVERE, null, ex);
+		}
 
-        Charge charge = Charge.create(params);
-        return charge.getStatus();
-    }
+		return status;
+	}
+
+	public static String charge(Integer amount, String cardNumber, String expMonth, String expYear, String customerID)
+			throws AuthenticationException, InvalidRequestException, APIConnectionException, CardException,
+			APIException {
+
+		Map<String, Object> params = new HashMap<>();
+		params.put("amount", amount);
+
+		if (customerID != null) {
+			params.put("customer", customerID);
+		} else {
+			Map<String, String> card = new HashMap<>();
+			card.put("object", "card");
+			card.put("number", cardNumber);
+			card.put("exp_year", expYear);
+			card.put("exp_month", expMonth);
+			params.put("source", card);
+		}
+
+		params.put("currency", "usd");
+
+		Charge charge = Charge.create(params);
+		return charge.getStatus();
+	}
+
+	public static String charge(Integer amount, String customerId, String cardId) throws AuthenticationException,
+			InvalidRequestException, APIConnectionException, CardException, APIException {
+		Map<String, Object> params = new HashMap<>();
+		params.put("amount", amount);
+
+		params.put("customer", customerId);
+
+		params.put("card", cardId);
+
+		params.put("currency", "usd");
+
+		Charge charge = Charge.create(params);
+		return charge.getStatus();
+	}
 }
--- src/com/bathwater/services/TwilioSMSService.java
+++ src/com/bathwater/services/TwilioSMSService.java
@@ -21,32 +21,30 @@ import com.twilio.sdk.resource.instance.Message;
  * @author rajeshk
  */
 public class TwilioSMSService {
-    
-//    private static final String ACCOUNT_SID = System.getProperty("TWILIO_SID");
-//    private static final String AUTH_TOKEN = System.getProperty("TWILIO_TOKEN");
-    private static final String ACCOUNT_SID = "AC668e11ac80aa574ae1cc3c7071c1d4f0";
-    private static final String AUTH_TOKEN = "084748a885b21237546d152f29696f33";
-    
-    public static boolean sendSMS(String body, String to) {
-        TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
-        
-        List<NameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("From", "+19177460268"));
-        if (!to.startsWith("+")) {
-            params.add(new BasicNameValuePair("To", "+"+to));
-        } else {
-            params.add(new BasicNameValuePair("To", to));
-        }
-        params.add(new BasicNameValuePair("Body", body));
-        
-        MessageFactory messageFactory = client.getAccount().getMessageFactory();
-        try {
-            Message message = messageFactory.create(params);
-        } catch (TwilioRestException ex) {
-            return false;
-        }
-        
-        return true;
-    }
-    
+
+	private static final String ACCOUNT_SID = System.getProperty("TWILIO_SID");
+	private static final String AUTH_TOKEN = System.getProperty("TWILIO_TOKEN");
+
+	public static boolean sendSMS(String body, String to) {
+		TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+
+		List<NameValuePair> params = new ArrayList<>();
+		params.add(new BasicNameValuePair("From", "+19177460268"));
+		if (!to.startsWith("+")) {
+			params.add(new BasicNameValuePair("To", "+" + to));
+		} else {
+			params.add(new BasicNameValuePair("To", to));
+		}
+		params.add(new BasicNameValuePair("Body", body));
+
+		MessageFactory messageFactory = client.getAccount().getMessageFactory();
+		try {
+			Message message = messageFactory.create(params);
+		} catch (TwilioRestException ex) {
+			return false;
+		}
+
+		return true;
+	}
+
 }
